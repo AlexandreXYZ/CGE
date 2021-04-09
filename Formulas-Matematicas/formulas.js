b6a2288bc71fec5b5ade9acebf4850430cc18e0f
k1vz <mrnerd777@gmail.com>
2021-04-09 16:02:56 -0300

    :art: Converted 'Sunset' to new form and refactored 'diaAno()' function
    | Formulas.js |
    Refactored 'diaAno()' function - (Switched between 'if' to 'switch' method)
    
    | Index.html |
    Alterned order to display 'latitude' and 'longitude'
    Implements a button who will send user input

--- Formulas-Matematicas/formulas.js
+++ Formulas-Matematicas/formulas.js
@@ -1,47 +1,29 @@
-function diaAno(dia, mes1, bicesto=false) {
+function diaAno(dia, mes, bicesto=false) {
     // Cálculo que retorna o dia do ano. Ex: 108/365
     // Pode ser utilizado em qualquer ano, em anos bicestos é necessário o parâmetro 'true'.
-    var janMax = 31;
-    var abrMax = 31;
-    if(bicesto == true){var fevMax=29}
-    if(bicesto == false){var fevMax=28}
-    var marMax = 30;
-    var maiMax = 31;
-    var junMax = 30;
-    var julMax = 31;
-    var agoMax = 31;
-    var setMax = 30;
-    var outMax = 31;
-    var novMax = 30;
-    var dezMax = 31;
+    var jan = mar = mai = jul = ago = out = dez = 31;
+    var abr = jun = set = nov = 30;
+    switch(bicesto) {
+        case true: var fev = 29; break;
+        case false: var fev = 28; break;
+    }
     
-    var jan = 0;        
-    var fev = 0;
-    var abr = 0;
-    var mar = 0;
-    var mai = 0;
-    var jun = 0;
-    var jul = 0;
-    var ago = 0;
-    var set = 0;
-    var out = 0;
-    var nov = 0;
-    var dez = 0;
-    
-    if(mes1 == "jan"){var mes = jan;}
-    if(mes1 == "fev"){var mes = fev + janMax;}
-    if(mes1 == "mar"){var mes = mar + janMax + fevMax;}
-    if(mes1 == "abr"){var mes = abr + janMax + fevMax + marMax;}
-    if(mes1 == "mai"){var mes = mai + janMax + fevMax + marMax + abrMax;}
-    if(mes1 == "jun"){var mes = jun + janMax + fevMax + marMax + abrMax + maiMax;}
-    if(mes1 == "jul"){var mes = jul + janMax + fevMax + marMax + abrMax + maiMax + junMax;}
-    if(mes1 == "ago"){var mes = ago + janMax + fevMax + marMax + abrMax + maiMax + junMax + julMax;}
-    if(mes1 == "set"){var mes = set + janMax + fevMax + marMax + abrMax + maiMax + junMax + julMax + agoMax;}
-    if(mes1 == "out"){var mes = out + janMax + fevMax + marMax + abrMax + maiMax + junMax + julMax + agoMax + setMax;}
-    if(mes1 == "nov"){var mes = nov + janMax + fevMax + marMax + abrMax + maiMax + junMax + julMax + agoMax + setMax + outMax;}
-    if(mes1 == "dez"){var mes = dez + janMax + fevMax + marMax + abrMax + maiMax + junMax + julMax + agoMax + setMax + outMax + novMax;}
+    switch(mes) {
+        case 01: var tot_meses = 0; break;
+        case 02: var tot_meses = jan; break;
+        case 03: var tot_meses = jan + fev; break;
+        case 04: var tot_meses = jan + fev + mar; break;
+        case 05: var tot_meses = jan + fev + mar + abr; break;
+        case 06: var tot_meses = jan + fev + mar + abr + mai; break;
+        case 07: var tot_meses = jan + fev + mar + abr + mai + jun; break;
+        case 08: var tot_meses = jan + fev + mar + abr + mai + jun + jul; break;
+        case 09: var tot_meses = jan + fev + mar + abr + mai + jun + jul + ago; break;
+        case 10: var tot_meses = jan + fev + mar + abr + mai + jun + jul + ago + set; break;
+        case 11: var tot_meses = jan + fev + mar + abr + mai + jun + jul + ago + set + out; break;
+        case 12: var tot_meses = jan + fev + mar + abr + mai + jun + jul + ago + set + out + nov; break;
+    }
 
-    return dia + mes;
+    return dia + tot_meses;
 }
 
 function conv_degree(degree) {
@@ -53,7 +35,7 @@ function declinacao(dia_seq) {
     //original: 23.45 * Math.sin(conv_degree((360/365)*(284 + this.getDia_seq())))
 
     //usp
-    var declinacao = 23.45 * Math.sin(conv_degree(360*(dia_seq - 80) / 365));
+    var declinacao = 23.45 * Math.sin(conv_degree(360 * (dia_seq - 80) / 365));
     return declinacao;
 }
 
@@ -95,5 +77,5 @@ function corret_lati(longitude, schedule_original) {
     }
 
     // Saída (output)
-    return (schedule_adjusted);
+    return schedule_adjusted;
 }
