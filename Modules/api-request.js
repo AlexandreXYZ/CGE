82568e1f5ae0f2429b16c9d2875f32325270f0eb
k1vz <mrnerd777@gmail.com>
2021-04-29 15:01:25 -0300

    :construction: added output for city name
    Added a check to see if city isn't null

--- Modules/api-request.js
+++ Modules/api-request.js
@@ -1,19 +1,16 @@
 function cityName(lat, lng) {
-    fetch('https://api.teleport.org/api/locations/' + lat +' %2C%20' + lng + '/', {
-        mode: 'cors'
-    })
-    .then(response => response.json())
-    .then(data => {
-        var name = "No cities found.";
-
-        if (!data.http_status_code) {
-            var name = data._embedded["location:nearest-cities"][0]._links["location:nearest-city"].name;
-        }
-        
-        console.log(name);
-        // return (name);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+    fetch('https://api.teleport.org/api/locations/' + lat + '%2C%20' + lng + '/', {
+            mode: 'cors'
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data._embedded["location:nearest-cities"] != null) {
+                document.getElementById("cidade").innerHTML = data._embedded["location:nearest-cities"][0]._links["location:nearest-city"].name;
+            } else {
+                document.getElementById("cidade").innerHTML = "No cities found";
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
