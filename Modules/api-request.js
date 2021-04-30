e1651dbebd0ee78177e6649ecf23d41bc15d1bdd
k1vz <mrnerd777@gmail.com>
2021-04-30 16:19:59 -0300

    :rocket: city field working like input too
    If the user send city and not coordinates, the algorithm will request to API, the coordinates from that city
    If the user send coordinates and not city, the algorithm will request to API, the name of that city
    If the user sends everything, the API will not be requested

--- Modules/api-request.js
+++ Modules/api-request.js
@@ -1,5 +1,5 @@
 function cityName(lat, lng) {
-    fetch('https://api.teleport.org/api/locations/' + lat + '%2C%20' + lng + '/', {
+    fetch('https://api.teleport.org/api/locations/' + lat + '%2C%20' + lng, {
             mode: 'cors'
         })
         .then(response => response.json())
@@ -9,19 +9,47 @@ function cityName(lat, lng) {
                 var city = cityProps.name;
                 var cityID = cityProps.href.slice(-8, -1);
 
-                fetch('https://api.teleport.org/api/cities/geonameid:' + cityID + '/', {
+                fetch('https://api.teleport.org/api/cities/geonameid:' + cityID, {
                         mode: 'cors'
                     })
                     .then(response => response.json())
                     .then(data => {
                         var country = data._links["city:country"].href.slice(-3, -1);
-                        document.getElementById("cidade").innerHTML = city + ', ' + country
+                        document.getElementById("cidade").value = city + ', ' + country;
                     });
             } else {
-                document.getElementById("cidade").innerHTML = "No cities found";
+                document.getElementById("cidade").value = "No cities found";
             }
         })
-        .catch((error) => {
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function cityCoords(cityName) {
+    fetch('https://api.teleport.org/api/cities/?search=' + cityName, {
+            mode: 'cors'
+        })
+        .then(response => response.json())
+        .then(data => {
+            var cityID = data._embedded["city:search-results"][0]._links["city:item"].href.slice(-8, -1);
+
+            fetch('https://api.teleport.org/api/cities/geonameid:' + cityID, {
+                    mode: 'cors'
+                })
+                .then(response => response.json())
+                .then(data => {
+                    var coords = {
+                        lat: data.location.latlon.latitude,
+                        lng: data.location.latlon.longitude
+                    }
+
+                    document.getElementById("latitude").value = coords.lat;
+                    document.getElementById("longitude").value = coords.lng;
+                    document.getElementById("submit_button").click();
+                });
+        })
+        .catch(error => {
             console.error('Error:', error);
         });
 }
