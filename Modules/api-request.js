862bec543d690ecc6d4ab5ef6ea0bc39fbfda885
k1vz <mrnerd777@gmail.com>
2021-05-16 14:15:23 -0300

    :bug: solved 'NaN' error

--- Modules/api-request.js
+++ Modules/api-request.js
@@ -7,7 +7,7 @@ function cityName(lat, lng) {
             if (data._embedded["location:nearest-cities"] != null) {
                 var cityProps = data._embedded["location:nearest-cities"][0]._links["location:nearest-city"];
                 var city = cityProps.name;
-                var cityID = cityProps.href.slice(-8, -1);
+                var cityID = cityProps.href.slice(46, -1);
 
                 fetch('https://api.teleport.org/api/cities/geonameid:' + cityID, {
                         mode: 'cors'
