686c7eeff2883d4bb31292ccde7d2d4133dba0e4
k1vz <mrnerd777@gmail.com>
2021-10-27 13:11:42 -0300

    graphics working correctly

--- graphics.js
+++ graphics.js
@@ -1,28 +1,51 @@
-function graphics(id, x1, y1, x2, y2){
+function graphics(id, name, dadosVirtuais, dadosReais) {
+	dadosVirtuais.type = 'lines';
+	dadosVirtuais.name = 'Gnômon Virtual';
+	dadosReais.type = 'lines';
+	dadosReais.name = 'Gnômon Real';
 
-  var trace1 = {
-    x: x1,
-    y: y1,
-    type: 'scatter'
-  };
+	var data = [dadosVirtuais, dadosReais];
 
-  var trace2 = {
-    x: x2,
-    y: y2,
-    type: 'scatter'
-  };
-
-  var data = [trace1, trace2];
-
-  var layout = {
-    xaxis: {
-      type: 'log',
-      autorange: true
-    },
-    yaxis: {
-      type: 'log',
-      autorange: true
-    }
-  };
-  Plotly.newPlot(id, data, layout);
+	var layout = {
+		title: name,
+		xaxis: {
+			title: 'Horário'
+		}
+	};
+	Plotly.newPlot(id, data, layout);
 }
+
+const horario = ['15:00', '15:30', '16:00', '16:30'];
+const dadosReais = {
+	x: {
+		x: horario,
+		y: [0.687, 0.771, 0.841, 0.897]
+	},
+	y: {
+		x: horario,
+		y: [0.126, 0.080, 0.028, -0.027]
+	},
+	z: {
+		x: horario,
+		y: [0.716, 0.632, 0.540, 0.441]
+	}
+};
+
+const dadosVirtuais = {
+	x: {
+		x: horario,
+		y: [0.149, 0.161, 0.178, 0.126]
+	},
+	y: {
+		x: horario,
+		y: [0.035, 0.090, 0.018, 0.022]
+	},
+	z: {
+		x: horario,
+		y: [0.034, 0.048, 0.024, 0.041]
+	}
+};
+
+graphics('graphicX', 'Coordenadas X', dadosReais.x, dadosVirtuais.x)
+graphics('graphicY', 'Coordenadas Y', dadosReais.y, dadosVirtuais.y)
+graphics('graphicZ', 'Coordenadas Z', dadosReais.z, dadosVirtuais.z)
