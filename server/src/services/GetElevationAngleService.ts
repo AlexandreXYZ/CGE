abd25fa5566d6351aea22303508774ce49f6bff3
k1vz <mrnerd777@gmail.com>
2021-11-03 17:38:32 -0300

    removed comments

--- server/src/services/GetElevationAngleService.ts
+++ server/src/services/GetElevationAngleService.ts
@@ -15,14 +15,3 @@ export class GetElevationAngleService {
 		return elevationAngle
 	}
 }
-
-// ({lat: 3.633056,lng: 6.543333}, {hora: 15,min: 0,seg: 0}, {dia: 29,mes: 10,ano: 2018})
-
-//declination (rad) = -0.2573398621604525
-//declination (deg) = -14.744
-
-//elevation (rad) = 0.729263341449354
-//elevation (deg) = 41.78
-
-//azimuth (rad) = 4.302243465326511
-//azimuth (deg) = 246.50
