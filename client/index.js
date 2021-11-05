a131de52b1f4ff4db5bca28d55a173a9216f9ff9
k1vz <mrnerd777@gmail.com>
2021-11-05 16:59:11 -0300

    solved merge conflict (working - tested)

--- client/index.js
+++ client/index.js
@@ -15,14 +15,7 @@ function getValues() {
         min: parseInt((document.getElementById("time").value).split(":")[1])
     };
 
-<<<<<<< HEAD:index.js
-    // /* ------- */ let cge = new Calibrator({lat: 3.633056,lng: 6.543333}, {hour: 15,min: 0}, {day: 29,month: 10,year: 2018}); //4debug
-    // /*solarsena*/ let cge = new Calibrator({lat: 32.22,lng: -110.9756}, {hour: 14,min: 00}, {day: 02,month: 03,year: 2020}); //4debug
     let cge = new Calibrator(coord, time, date);
-=======
-    let a = new Calibrador(coord, horario, data);
-    // console.log(coord, horario, data);
->>>>>>> feature/api:client/index.js
 
     if (!time.hour || !date.day) {
         alert("Missing parameters!");
