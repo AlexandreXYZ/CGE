096b13ef8ab44112382e50dae0e424967e5475fa
k1vz <mrnerd777@gmail.com>
2021-11-25 18:28:54 -0300

    new route '/coordinates/now' implemented
    This route will return all the coordinates of the user time, using default latitude (IFC)

---
+++ server/src/controllers/GetCoordinatesNowController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express"
+import { GetCoordinatesService } from "../services/GetCoordinatesService"
+import { GetDatesService } from "../services/GetDatesService"
+
+export class GetCoordinatesNowController {
+	async handle(request: Request, response: Response) {
+		try {
+			
+			const { dateISO } = request.body
+			
+			const latitude = -29.101912006
+			const getDatesService = new GetDatesService()
+			const getCoordinatesService = new GetCoordinatesService()
+
+			const { sequentialDay, time } = getDatesService.localDate(dateISO)
+			const coordinates = await getCoordinatesService.execute(sequentialDay, time, latitude, dateISO)
+
+			return response.json(coordinates)
+		}
+		catch(err) {
+			throw Error(err)
+		}
+	}
+}
